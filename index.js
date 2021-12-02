6273badeb2021832151de3e0299b6097b88e5978
christian <cktshukudu@gmail.com>
2021-12-02 11:36:15 +0200

    added index page and style

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 let express = require('express');
 let app = express();
-
-
-
-
 const exphbs = require('express-handlebars');
 
 const bodyParser = require('body-parser');
 const path = require('path');
 
 
-let PORT = process.env.PORT || 3003;
+let PORT = process.env.PORT || 3001;
 
 //Configure the express-handlebars module
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -18,18 +14,34 @@ app.set('view engine', 'handlebars');
 
 const session = require('express-session');
 const { compile } = require('handlebars');
-const { query } = require('express');
+
+//Set-up middleware
 
 
 
+//app.use(fav(path.join(__dirname, 'public', 'img/favicon.ico')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'));
 
 
-  
 
-  
-  app.listen(PORT, function () {
-    console.log('App starting on port', PORT);
+  app.get('', (req, res) => {
+    res.render('home');
   });
+
+  app.post('', (req, res) => {
+    res.redirect('');
+  });
+
+
+
+ 
+
+
+ 
+
+app.listen(PORT, function () {
+  console.log('App starting on port', PORT);
+});
+
